d5d745f6dd12732548e41833ad378f8f89f27044
antontx <tarisjan@gmail.com>
2023-11-18 20:58:39 +0100

    fixes

--- Aufgabe5/solution/aufg5.py
+++ Aufgabe5/solution/aufg5.py
@@ -10,20 +10,25 @@ tour = df.values.tolist()
 print(df)
 
 
+def print_subtour(subtour_indices, tour):
+    subtour = [tour[idx][0] for idx in subtour_indices]
+    print(" - ".join(subtour))
+
 def find_subtours(tour):
-    subtours = []
+    subtour_stops = []
     open_stops = {}
-    for i,stop in enumerate(tour):
+    for i, stop in enumerate(tour):
         if stop[0] in open_stops:
-            subtours += list(range(open_stops[stop[0]][1]+1,i+1))
+            subtour_indices = list(range(open_stops[stop[0]][1], i + 1))
+            print_subtour(subtour_indices, tour)
+            subtour_stops += subtour_indices[1:]
             open_stops.clear()
         elif stop[2] == "X":
             open_stops.clear()
-            open_stops[stop[0]] = (stop[3],i)
+            open_stops[stop[0]] = (stop[3], i)
         else:
-            open_stops[stop[0]] = stop[3]
-    
-    return subtours
+            open_stops[stop[0]] = (stop[3], i)
+    return subtour_stops
 
 
 def remove_stops(tour: list,stop_indices):
@@ -32,6 +37,6 @@ def remove_stops(tour: list,stop_indices):
     return tour
 
 
-stops = find_subtours(tour)
-new_tour = remove_stops(tour,stops)
+subtour_stops = find_subtours(tour)
+new_tour = remove_stops(tour,subtour_stops)
 print(pd.DataFrame(new_tour))
